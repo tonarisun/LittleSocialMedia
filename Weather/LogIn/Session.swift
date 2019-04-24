e449517b6120ed0dce1c866c4c0dc62f202b6530
tonarisun <pozitive@inbox.ru>
2019-04-24 11:40:53 +0700

    add singleton Session

---
+++ Weather/LogIn/Session.swift
@@ -0,0 +1,20 @@
+//
+//  Session.swift
+//  Weather
+//
+//  Created by Olga Lidman on 24/04/2019.
+//  Copyright © 2019 Home. All rights reserved.
+//
+
+import UIKit
+
+class Session {
+    
+    static let session = Session()
+    
+    let token : String = ""
+    let userID : Int = 0
+    
+    private init(){}
+    
+}
